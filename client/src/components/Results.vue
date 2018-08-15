f99ca4e5c2f1e13f8606b6ce47607eaa3e98b53a
Victor Tao <platers81@gmail.com>
2018-08-15 13:26:28 -0400

    functional?

--- client/src/components/Results.vue
+++ client/src/components/Results.vue
@@ -2,11 +2,16 @@
   <div class="posts">
     <h1>Lowest number</h1>
     <p>Guess a positive integer. The person with the lowest unique number wins!</p>
-    <ul>
-        <li v-for="event in events" v-bind:key="event.id">
-            {{event.game}}, {{event.name}}, {{event.number}}
-        </li>
-    </ul>
+    <div class="table">
+      <table>
+          <tr v-for="event in events" v-bind:key="event.id" v-bind:class="event.color ? 'white' : 'gray'">
+              
+              <td>{{event.name}}</td>
+              <td>{{event.number}}</td>
+              <td v-if="event.winner">WINN</td>
+          </tr>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -25,7 +30,28 @@ export default {
   methods: {
     async getEvents () {
       const response = await EventService.fetchEvents({})
-      this.events = response.data.events
+      const events = response.data.events
+      var winners = {}
+      for (var i = events.length - 1; i >= 0; i--) {
+        if (i > 0 && events[i].number === events[i - 1].number && events[i].game === events[i - 1].game) {
+          continue
+        }
+        if (i < events.length && events[i].number === events[i + 1].number && events[i].game === events[i + 1].game) {
+          continue
+        }
+        winners[events[i].game] = events[i].number
+      }
+      for (var e of events) {
+        this.events.push(
+          {
+            name: e.name,
+            game: e.game,
+            number: e.number,
+            color: e.game % 2,
+            winner: winners[e.game] === e.number
+          }
+        )
+      }
       // this.$router.push({ name: 'Posts' })
     },
     async currentGame () {
@@ -45,8 +71,15 @@ export default {
   outline: none;
   font-size: 12px;
 }
-.form div {
-  margin: 20px;
+table {
+
+}
+.table {
+  margin: 0 auto;
+  width: 200px; 
+}
+.gray {
+  background-color: #D3D3D3;
 }
 .app_post_btn {
   background: #4d7ef7;
