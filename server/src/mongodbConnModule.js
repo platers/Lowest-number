ea3db7b75c28d5b56aa6976b772eaea51b9d3eb6
Victor Tao <platers81@gmail.com>
2018-08-17 17:11:24 -0400

    using mlab

--- server/src/mongodbConnModule.js
+++ server/src/mongodbConnModule.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 module.exports.connect = function() {
-	mongoose.connect('mongodb://localhost:27017/MEVN-boilerplate');
+
+	mongoose.connect('mongodb://heroku_tf2jzmnc:9q7cc13p85v5orgu12o1ucoehp@ds125912.mlab.com:25912/heroku_tf2jzmnc');
 	var db = mongoose.connection;
 	db.on("error", console.error.bind(console, "connection error"));
 	db.once("open", function(callback){
