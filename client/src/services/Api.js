a344901e0d1549b9bd6802f4224d6004feba7c97
Victor Tao <platers81@gmail.com>
2018-08-17 17:52:00 -0400

    Changed environment vars

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default() => {
   return axios.create({
-    baseURL: `http://lowest-number.herokuapp.com/`
+    baseURL: process.env.baseURL || `http://localhost:8081`
   })
 }
