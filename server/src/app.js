a683976b7bf2cef80049a3b5bf469ec82e15e560
Victor Tao <platers81@gmail.com>
2018-08-16 21:00:16 -0400

    build to dist

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json())
 app.use(cors())
 app.disable('etag');
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('../../client/build'));
+	app.use(express.static('../../client/dist'));
 }
 const mongodb_conn_module = require('./mongodbConnModule');
 var db = mongodb_conn_module.connect();
